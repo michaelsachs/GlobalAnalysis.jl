996bf43eddf39cfbd167414f252b510584df01ee
Michael Sachs <mrs314@ic.ac.uk>
2021-03-15 17:26:54 +0000

    export importData function

--- src/GlobalAnalysis.jl
+++ src/GlobalAnalysis.jl
@@ -1,15 +1,13 @@
 module GlobalAnalysis
 
+export importData
+
 include("TypeDefinitions.jl")
 include("DataImport.jl")
-
-#using .TypeDefinitions
-using .DataImport
 
 directory = raw"C:\Box Sync\Data\fs-TAS\Matlab data\Fe2O3\400exc\test"
 
 Data = importData(directory)
 
-Data = combineData(Data)
 
 end #module
