2bc9c4a4769b3ee7ad8462ddbf0332ae010395fe
jessicaflowers <108426511+jessicaflowers@users.noreply.github.com>
2023-08-14 14:34:02 -0700

    Update KineticModel.jl
    add residual maps to file

--- src/KineticModel.jl
+++ src/KineticModel.jl
@@ -2,48 +2,46 @@ using Catalyst, Plots, DifferentialEquations
 using BlackBoxOptim
 using CSV
 using DataFrames
-# using DataStructures
+using DataStructures
+using Statistics
+using NaNStatistics
+
 include("IRFConvolution.jl")
 include("DataImport.jl")
 include("TypeDefinitions.jl")
 ####### ONLY EDIT BETWEEN THESE LINES #######
 
-# da = CSV.read("/Users/jessicaflowers/Desktop/JULIA/data/Fe2O3_0.20.csv", DataFrame)
-testData = importData("/Users/jessicaflowers/Desktop/JULIA/data/")
+RealData = importData("/Users/jessicaflowers/Desktop/JULIA/data/CdO/"; miss="NaN")
+RealData = RealData[1]
+
 # define a kinetic model
 rs = @reaction_network begin
-    k1, A --> 0
-    k2, 2B --> 0
-    k3, C --> 0
+    k1, A --> B
+    k2, B --> 0
 end
 
 # define bounds for the parameter optimization 
-state_lower = [1, 1, 1] # A, B, C
-state_upper = [1, 1, 1] # for first order, set the first value to 1 and the rest to 0
+state_lower = [1, 0] # A, B, C
+state_upper = [1, 0] 
 
-IRF_lower = [0.3, 0.04]
-IRF_upper = [0.5, 0.06]
+IRF_lower = [0.1, 0.01]
+IRF_upper = [0.2, 0.08]
 
-rate_const_lower = [0.01, 0.001, 0.00001]
-rate_const_upper = [0.9, 0.05, 0.00005]
+rate_const_lower = [1, 0.0001]
+rate_const_upper = [3, 0.0003]
+
 
 ####### ONLY EDIT BETWEEN THESE LINES #######
 
 # some pre-processing of the input csv file
-# convert dataframe into matrix
-dat = Matrix(da)
-frequencies = dat[:,1]
-data = dat[:, 1:end .≠ 1] # get rid of 1st column as this is just the freq range, not real data
+frequencies = RealData.x
+# frequencies = vcat(frequencies...)
+time = RealData.y
+# time = vcat(time...)
+Data = RealData.z
+# Data = (vcat(Data...))
+# Data = coalesce.(Data, NaN)
 
-
-function parse_time_column(column_name::String)
-    parsed_string = split(column_name, "_")[1]  # Take only the part before "_"
-    return parse(Float64, parsed_string)
-end
-
-# Extract the time stamps and convert them into a vector of Float64
-col_names = names(da)
-time_stamp = [parse_time_column(col_name) for col_name in col_names[2:end]]
 
 # create bounds array
 lower = vcat(state_lower, IRF_lower, rate_const_lower)
@@ -53,16 +51,6 @@ bounds = Array{Tuple{Float64, Float64}}(undef,length(lower))
 for k in 1:length(lower)
     bounds[k] = (lower[k], upper[k])
 end
-
-TimeDiff = diff(time_stamp)
-IRFStep = minimum(filter(t -> t > 0, TimeDiff))
-IRFWindow = 30*σ
-TimeIRFPos = collect(IRFStep/2:IRFStep:IRFWindow)
-IRFWindowCut = IRFWindow*0.5
-TimeODE = sort(unique([TimeIRFPos; time_stamp[time_stamp .> IRFWindowCut] .- μ]))
-TimeODEBool = [in.(TimeODE, Ref(Set(TimeIRFPos))), in.(TimeODE, Ref(Set(time_stamp .- μ)))]
-
-tspan = [minimum(TimeODE),maximum(TimeODE)] 
 
 # function to produce data with "guessed" parameters
 function GetData(param) # A, B, ..., μ, σ, k1, k2, k3
@@ -108,38 +96,115 @@ function GetData(param) # A, B, ..., μ, σ, k1, k2, k3
     μ=param[num_states+1]
     σ=param[num_states+2]
 
+    TimeODE, TimeIRFPos, IRFWindowCut, TimeODEBool = defineIRFTime(time, μ, σ)
+    tspan = [minimum(TimeODE),maximum(TimeODE)] 
+
     prob = ODEProblem(rs, u0, tspan, p; saveat=TimeODE)
     sol  = solve(prob, Tsit5())
     sol_array = Array(sol)
-    # kinODE = vcat(sol.u...)
+
     # Perform convolution for each component
     ConvKin = Vector{Array{Float64,1}}(undef, num_states)
     for i in 1:num_states # loop through components
         component = sol_array[i,:]
-        ConvKin[i] = GlobalAnalysis.convolveIRF(time_stamp, component, μ, σ, TimeIRFPos, IRFWindowCut, TimeODEBool)
+        ConvKin[i] = convolveIRF(time, component, μ, σ, TimeIRFPos, IRFWindowCut, TimeODEBool)
     end
 
     KinMatrix = reduce(vcat, ConvKin')  # kinetic matrix
-    
-    TestSpec = data / KinMatrix  # spectral signatures matrix
+    TestSpec = Data / KinMatrix  # spectral signatures matrix
     testData = TestSpec * KinMatrix # test matrix from kinetics and signatures
-
     return testData, TestSpec, KinMatrix
 end
 
 # objective function which returns the sum of squared differences bewteen 
 # the produced data with guessed parameters (GetData) and the real data matrix
-function Objective(param)
+function Objective(param; output="res")
     da = GetData(param)
     testData = da[1]
-    return sum(abs2, testData .- data)
+    if output == "res"
+        return nansum((testData .- Data).^2) #sum(abs2, testData .- Data)
+    elseif output == "map"
+        return heatmap(time, frequencies, (testData - Data), xlabel="Time", ylabel="Wavelength", 
+                    title="Residuals Map", colorbar_title="\n \n \n Δ Absorbance", 
+                    right_margin=15Plots.mm, left_margin=10Plots.mm, xguidefontsize=10, yguidefontsize=10,
+                    c = :thermal)
+    end
 end
 
 # minimize objective function to optimize parameters
-OP = bboptimize(Objective; SearchRange = bounds)
+# OP = bboptimize(Objective; SearchRange = bounds)
 RecoveredParam = best_candidate(OP)
 
 RecoveredData = GetData(RecoveredParam)
 DataMatrix = RecoveredData[1] # data matrix
 RecoveredSpec = RecoveredData[2] # spectral signatures
 RecoveredKinetics = RecoveredData[3] # kinetic traces
+
+# kinetics
+tpos = time.>0
+plot(time[tpos], RecoveredKinetics[1,:][tpos], xscale=:log10,
+     title="Recovered Kinetics", xlabel="log(Time)", legend=:topleft, linewidth = 2)
+plot!(time[tpos], RecoveredKinetics[2,:][tpos], xscale=:log10, linewidth = 2)
+# plot!(time[tpos], RecoveredKinetics[3,:][tpos], xscale=:log10, linewidth = 2)
+
+
+
+# spectral signatures
+plot(frequencies, RecoveredSpec, xlabel="Wavelength", linewidth = 2) 
+
+plot(frequencies, RecoveredSpec, title="Recovered Spectral Signatures", xlabel="Wavelength", linewidth = 2) 
+
+# 3D data 
+surface(time, frequencies, DataMatrix, title="Recovered Data", xlabel="Time", ylabel="         Wavelength", xguidefontsize=10, yguidefontsize=10, xrotation = 45, right_margin=15Plots.mm, colorbar_title="\n\n\n\nΔ Absorbance")
+surface(time, frequencies, Data, title="Real Data", xlabel="Time", ylabel="         Wavelength", xguidefontsize=10, yguidefontsize=10, xrotation = 45, right_margin=15Plots.mm, colorbar_title="\n\n\n\nΔ Absorbance")
+
+# residual maps 
+map = Objective(RecoveredParam; output="map")
+
+real_heatmap = heatmap(time, frequencies, Data, 
+    xlabel="Time", ylabel="Wavelength", title="Real Data", 
+    colorbar_title="\n\nΔ Absorbance", right_margin=15Plots.mm, left_margin=10Plots.mm, 
+    xguidefontsize=10, yguidefontsize=10,
+    c = :thermal)
+optim_heatmap = heatmap(time, frequencies, DataMatrix, 
+    xlabel="Time", ylabel="Wavelength", title="Recovered Data" ,
+    colorbar_title="\n\nΔ Absorbance", right_margin=15Plots.mm, left_margin=10Plots.mm, 
+    xguidefontsize=10, yguidefontsize=10,
+    c = :thermal)
+
+residual_val = Objective(RecoveredParam; output="res")
+
+# # Normalize the spectral signatures and kinetics
+
+# kn1 = RecoveredKinetics[1,:]
+# kn2 = RecoveredKinetics[2,:]
+# kn3 = RecoveredKinetics[3,:]
+
+# max1 = maximum(kn1)
+# max2 = maximum(kn2)
+# max3 = maximum(kn3)
+
+
+# kn1 = kn1 / max1
+# kn2 = kn2 / max2
+# kn3 = kn3 / max3
+
+# plot(kn1)
+# plot!(kn2)
+# plot!(kn3)
+
+# spc1 = RecoveredSpec[:,1]
+# spc2 = RecoveredSpec[:,2]
+# spc3 = RecoveredSpec[:,3]
+
+# Max1 = maximum(filter(!isnan, spc1))
+# Max2 = maximum(filter(!isnan, spc2))
+# Max3 = maximum(filter(!isnan, spc3))
+
+# spc1 = spc1 / Max1
+# spc2 = spc2 / Max2
+# spc3 = spc3 / Max3
+
+# plot(spc1)
+# plot!(spc2)
+# plot!(spc3)
