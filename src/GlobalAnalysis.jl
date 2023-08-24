39641931e9733c44cbc8cacf6b0ed5c512163f7f
michaelsachs <43791096+michaelsachs@users.noreply.github.com>
2023-08-24 15:37:05 -0700

    export only jupyter

--- src/GlobalAnalysis.jl
+++ src/GlobalAnalysis.jl
@@ -1,16 +1,12 @@
 module GlobalAnalysis
 
-export importData, defineIRFTime, convolveIRF, maskData, jupyterNotebook, jupyterLab
+#export importData, defineIRFTime, convolveIRF, maskData, jupyterNotebook, jupyterLab
+export jupyterNotebook, jupyterLab
 
-include("TypeDefinitions.jl")
-include("DataImport.jl")
-include("IRFConvolution.jl")
+#include("TypeDefinitions.jl")
+#include("DataImport.jl")
+#include("IRFConvolution.jl")
 include("notebooks.jl")
-
-#directory = raw"C:\Box Sync\Data\fs-TAS\Matlab data\Fe2O3\400exc\test"
-
-#Data = importData(directory; miss="NaN")
-#Data = importData(directory)
 
 
 end #module
