0a69c27235697ba6c83c4a763df3fd8e48d8cf1e
Michael Sachs <mrs314@ic.ac.uk>
2023-12-13 11:37:11 -0800

    add plot defaults

---
+++ src/plots.jl
@@ -0,0 +1,15 @@
+using Plots
+
+#----------------------------------------------------------------------
+# Default plot layout
+#----------------------------------------------------------------------
+
+# default fonts
+fo1 = font(10,"Helvetica")
+fo2 = font(12,"Helvetica")
+
+# set default plot parameters
+default(linewidth=2, grid=false, frame=:box, fg_legend=:transparent,
+    tickfont=fo1, guidefont=fo2, titlefont=fo2, legendfont=fo1,
+    size=(500,400), legend=false, tick_direction=:out, dpi=300, 
+    plot_titlefont=fo2)
