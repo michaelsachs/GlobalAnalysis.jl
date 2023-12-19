f159b447ed91ad5331554f43d9d71d2c82712453
michaelsachs <43791096+michaelsachs@users.noreply.github.com>
2023-12-18 16:00:20 -0800

    do not export notebooks for now

--- src/GlobalAnalysis.jl
+++ src/GlobalAnalysis.jl
@@ -1,12 +1,12 @@
 module GlobalAnalysis
 
 #export importData, defineIRFTime, convolveIRF, maskData, jupyterNotebook, jupyterLab
-export jupyterNotebook, jupyterLab
+#export jupyterNotebook, jupyterLab
 
 #include("TypeDefinitions.jl")
 #include("DataImport.jl")
 #include("IRFConvolution.jl")
-include("notebooks.jl")
+#include("notebooks.jl")
 
 
 end #module
