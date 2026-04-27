2fd1612f14e8f683c16968eec224476d1d468477
Michael Sachs <michael.sachs@univie.ac.at>
2026-04-27 15:07:08 +0200

    reduce plot size in notebook

--- src/plots.jl
+++ src/plots.jl
@@ -8,8 +8,11 @@ using Plots
 fo1 = font(9,"Helvetica")
 fo2 = font(11,"Helvetica")
 
+# shared plot sizes
+defaultPlotSize = (500,350)
+
 # set default plot parameters
 default(linewidth=2, grid=false, frame=:box, fg_legend=:transparent,
     tickfont=fo1, guidefont=fo2, titlefont=fo2, legendfont=fo1,
-    size=(500,350), legend=false, tick_direction=:out, dpi=300, 
-    plot_titlefont=fo2)
+    size=defaultPlotSize, legend=false, tick_direction=:out, dpi=300,
+    html_output_format=:svg, plot_titlefont=fo2)
